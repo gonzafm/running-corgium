3db7111fff418b5ed22e4d16493da2b83a3c6333
Gonzalo Medina <gonzafm@gmail.com>
2026-01-05 15:00:14 -0600

    Basic gRPC client (#3)

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,15 +3,18 @@ from src.main import app
 
 client = TestClient(app)
 
+
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
+
 def test_read_hello_name():
     response = client.get("/hello/Gemini")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello Gemini"}
+
 
 def test_read_hello_another_name():
     response = client.get("/hello/World")
