e5f959cc773892c3ad1f2da0adbd7a99d78ab571
Gonzalo Medina <gonzafm@gmail.com>
2026-01-27 14:31:54 -0600

    Fix warning in tests

--- src/database/postgres_service.py
+++ src/database/postgres_service.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta, timezone
 from pydantic import ValidationError
 from stravalib.model import SummaryActivity
 
+from src.config import settings
+
 
 class PostgresService:
     def __init__(self) -> None:
@@ -14,7 +16,11 @@ class PostgresService:
 
     async def _get_connection(self) -> asyncpg.Connection:
         return await asyncpg.connect(
-            user="rc-admin", password="password", database="postgres", host="127.0.0.1"
+            host=settings.db_host,
+            port=settings.db_port,
+            user=settings.db_user,
+            password=settings.db_password,
+            database=settings.db_name,
         )
 
     async def initialize(self) -> None:
